3da42ed00855085e7dc021e2c0529782d88f98b0
Никита <79624445+SavenkoNikita@users.noreply.github.com>
2022-02-19 00:29:40 +0300

    Задача №2 На лекции мы рассматривали пример для военкомата. Сейчас мы знаем мы про его рост. Расширить это приложение следующим условиями:
    1.Проверка на возраст призывника;
    2.Количество детей;
    3.Учится ли он сейчас.

--- Военкомат.py
+++ Военкомат.py
@@ -4,9 +4,7 @@ count_baby = 1
 student_now = False
 
 if 18 <= age <= 28:
-    if count_baby >= 2:
-        print('Grace period')
-    elif student_now is True:
+    if count_baby >= 2 or student_now is True:
         print('Grace period')
     else:
         if height < 170:
