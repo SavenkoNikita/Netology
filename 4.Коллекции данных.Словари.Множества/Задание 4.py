d5f1911509691717fd8453cb43b0e71a2ff1e357
Никита <79624445+SavenkoNikita@users.noreply.github.com>
2022-03-09 01:32:31 +0300

    Коллекции данных. Словари. Множества Задание 4

---
+++ 4.Коллекции данных.Словари.Множества/Задание 4.py
@@ -0,0 +1,10 @@
+stats = {'facebook': 55, 'yandex': 120, 'vk': 115, 'google': 99, 'email': 42, 'ok': 98}
+
+top_list = []
+
+for name, meaning in stats.items():
+    top_list.append([meaning, name])
+
+top_list.sort()
+
+print(top_list[-1][1])
