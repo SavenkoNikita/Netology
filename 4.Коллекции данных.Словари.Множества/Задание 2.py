1a3f2f2457da3e719dd4d7d0ad1baf589b65283f
Никита <79624445+SavenkoNikita@users.noreply.github.com>
2022-03-18 22:15:28 +0300

    Доработка

--- 4.Коллекции данных.Словари.Множества/Задание 2.py
+++ 4.Коллекции данных.Словари.Множества/Задание 2.py
@@ -2,9 +2,6 @@ ids = {'user1': [213, 213, 213, 15, 213],
        'user2': [54, 54, 119, 119, 119],
        'user3': [213, 98, 98, 35]}
 
-# list_id = [ids.values()]
-# print(list_id)
-
 list_id = []
 
 for elem in ids.values():
