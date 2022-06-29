60803e1d7d02ba258c5a951206734de3b9e9d39b
Nikita <SavenkoNikita@yandex.com>
2022-06-29 14:57:40 +0300

    YaDisk домашка

--- Работа с библиотекой requests, http запросы.py
+++ Работа с библиотекой requests, http запросы.py
@@ -2,13 +2,14 @@ from pprint import pprint
 
 import requests
 
-TOKEN = 'AQAAAAAIW_6wAADLW96xU0Wfq0PLv6PWycpdKTc'  # noqa
+
+# TOKEN = 'AQAAAAAIW_6wAADLW96xU0Wfq0PLv6PWycpdKTc'  # noqa
 
 
 class YandexDisk:
 
-    def __int__(self, token):
-        self.token = token
+    def __init__(self):
+        self.token = 'AQAAAAAIW_6wAADLW96xU0Wfq0PLv6PWycpdKTc'  # noqa
 
     def get_headers(self):
         return {
@@ -22,7 +23,26 @@ class YandexDisk:
         response = requests.get(files_url, headers=headers)
         return response.json()
 
+    def _get_upload_link(self, disk_file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': disk_file_path, 'overwrite': 'true'}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload_file_to_disk(self, disk_file_path, filename):
+        href_json = self._get_upload_link(disk_file_path=disk_file_path)
+        href = href_json.get['href']
+        pprint(href)
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()  # If status code is not 2##, break func, and error
+        if response.status_code == 201:
+            print('Success')
+
 
 if __name__ == '__main__':
     ya = YandexDisk()
-    pprint(ya.get_files_list())
+    # pprint(ya.get_files_list())
+    file_txt = '/home/nik/Рабочий стол/Netology/test_upload_file.txt'
+    ya.upload_file_to_disk('netology/Test_file.txt', file_txt)  # noqa
