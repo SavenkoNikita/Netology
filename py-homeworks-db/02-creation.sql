540b30b89192f7cc65e33be7de6143177c3939e5
Никита <79624445+SavenkoNikita@users.noreply.github.com>
2023-01-04 09:59:31 +0300

    Update 02-creation.sql

--- py-homeworks-db/02-creation.sql
+++ py-homeworks-db/02-creation.sql
@@ -1,7 +1,47 @@
 CREATE TABLE IF NOT EXISTS style (
-	genre_id SERIAL PRIMARY KEY,
-	name NOT NULL
+	genre_id INTEGER SERIAL PRIMARY KEY,
+	name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS musician (
-  
+	musician_id INTEGER SERIAL PRIMARY KEY,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS styles_musicians (
+	styles_musicians_id INTEGER SERIAL PRIMARY KEY,
+	genre_id INTEGER REFERENCES style(genre_id),
+	musician_id INTEGER REFERENCES musician(musician_id)
+);
+
+CREATE TABLE IF NOT EXISTS albums (
+	album_id INTEGER SERIAL PRIMARY KEY,
+	name TEXT NOT NULL
+	year_of_release DATE NOT NULL,
+	
+);
+
+CREATE TABLE IF NOT EXISTS albums_of_musicians (
+	albums_of_musicians_id INTEGER SERIAL PRIMARY KEY,
+	musician_id INTEGER REFERENCES musician(musician_id),
+	album_id INTEGER REFERENCES albums(album_id)
+);
+
+CREATE TABLE IF NOT EXISTS tracks (
+	track_id INTEGER SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	duration FLOAT NOT NULL,
+	album_id INTEGER REFERENCES albums(album_id)
+);
+
+CREATE TABLE IF NOT EXISTS collections (
+	collection_id INTEGER SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	year_of_release DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tracks_in_collection (
+	tracks_in_collection_id INTEGER SERIAL PRIMARY KEY,
+	collection_id INTEGER REFERENCES collections(collection_id),
+	track_id INTEGER REFERENCES tracks(track_id)
+);
