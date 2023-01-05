d1e97c3f7fee7a08f2fbca496da27b6d94ceb581
Никита <79624445+SavenkoNikita@users.noreply.github.com>
2023-01-05 13:26:16 +0300

    Update 03-dml(INSERT).sql

--- py-homeworks-db/03-dml(INSERT).sql
+++ py-homeworks-db/03-dml(INSERT).sql
@@ -1,1 +1,95 @@
+-- добавляем стили
+
+
+INSERT INTO style(name)
+VALUES('rock')
+
+INSERT INTO style(name)
+VALUES('hip-hop')
+
+INSERT INTO style(name)
+VALUES('Indie')
+
+INSERT INTO style(name)
+VALUES('jazz')
+
+INSERT INTO style(name)
+VALUES('electro')
+
+INSERT INTO style(name)
+VALUES('electronic rock')
+
+INSERT INTO style(name)
+VALUES('funk')
+
+INSERT INTO style(name)
+VALUES('pop-rock')
+
+INSERT INTO style(name)
+VALUES('rock `n` roll')
+
+INSERT INTO style(name)
+VALUES('pop')
+
+
+-- добавляем музыкантов
+
+
+INSERT INTO musician(name)
+VALUES('Nirvana')
+
+INSERT INTO musician(name)
+VALUES('Gorillaz')
+
+INSERT INTO musician(name)
+VALUES('Kanye West')
+
+INSERT INTO musician(name)
+VALUES('The Killers')
+
+INSERT INTO musician(name)
+VALUES('Coldplay')
+
+INSERT INTO musician(name)
+VALUES('Louis Armstrong')
+
+INSERT INTO musician(name)
+VALUES('Blank & Jones')
+
+INSERT INTO musician(name)
+VALUES('The Prodigy')
+
+INSERT INTO musician(name)
+VALUES('Michael Jackson')
+
+INSERT INTO musician(name)
+VALUES('The Beatles')
+
+INSERT INTO musician(name)
+VALUES('Elvis Aaron Presley')
+
+INSERT INTO musician(name)
+VALUES('Mariah Carey')
+
+
+--добавляем альбомы
+
+
+INSERT INTO albums(name, year_of_release)
+VALUES('Nevermind', '1992')
+
+INSERT INTO albums(name, year_of_release)
+VALUES('Demon Days', '2005')
+
+INSERT INTO albums(name, year_of_release)
+VALUES('Incesticide', '1988')
+
+INSERT INTO albums(name, year_of_release)
+VALUES('Gorillaz', '2001')
+
+INSERT INTO albums(name, year_of_release)
+VALUES('Hot Fuss', '2003')
+
+INSERT INTO albums(name, year_of_release)
+VALUES('Parachutes', '2000')
 
